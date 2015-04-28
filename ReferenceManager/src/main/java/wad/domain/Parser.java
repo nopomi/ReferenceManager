435d499ddc0292ab910cfb27c9c815c0c6608f16
nopomi <miska.noponen@gmail.com>
2015-04-28 17:55:11 +0300

    modified parser to remove string comparison with == operand

--- ReferenceManager/src/main/java/wad/domain/Parser.java
+++ ReferenceManager/src/main/java/wad/domain/Parser.java
@@ -43,25 +43,25 @@ public class Parser {
         builder.append("author = {" + articleRef.getAuthor() + e);
         builder.append("title = {" + articleRef.getTitle() + e);
         builder.append("year = {" + articleRef.getYear() + e);
-        if (articleRef.getJournal() != "" && articleRef.getJournal() != null) {
-            builder.append("journal = {" + articleRef.getJournal() + e);
+        if (!"".equals(articleRef.getJournal()) && articleRef.getJournal() != null) {
+            builder.append("journal = {").append(articleRef.getJournal()+e);
         }
-        if (articleRef.getVolume() != "" && articleRef.getVolume() != null) {
+        if (!"".equals(articleRef.getVolume()) && articleRef.getVolume() != null) {
             builder.append("volume = {" + articleRef.getVolume() + e);
         }
-        if (articleRef.getNumber() != "" && articleRef.getNumber() != null) {
+        if (!"".equals(articleRef.getNumber()) && articleRef.getNumber() != null) {
             builder.append("number = {" + articleRef.getNumber() + e);
         }
-        if (articleRef.getPages() != "" && articleRef.getPages() != null) {
+        if (!"".equals(articleRef.getPages()) && articleRef.getPages() != null) {
             builder.append("pages = {" + articleRef.getPages() + e);
         }
-        if (articleRef.getMonth() != "" && articleRef.getMonth() != null) {
+        if (!"".equals(articleRef.getMonth()) && articleRef.getMonth() != null) {
             builder.append("month = {" + articleRef.getMonth() + e);
         }
-        if (articleRef.getNote() != "" && articleRef.getNote() != null) {
+        if (!"".equals(articleRef.getNote()) && articleRef.getNote() != null) {
             builder.append("note = {" + articleRef.getNote() + e);
         }
-        if (articleRef.getKey() != "" && articleRef.getKey() != null) {
+        if (!"".equals(articleRef.getKey()) && articleRef.getKey() != null) {
             builder.append("key = {" + articleRef.getKey() + e);
         }
         builder.append(c);
@@ -75,28 +75,28 @@ public class Parser {
         builder.append("author = {" + bookRef.getAuthor() + e);
         builder.append("title = {" + bookRef.getTitle() + e);
         builder.append("year = {" + bookRef.getYear() + e);
-        if (bookRef.getPublisher() != "" && bookRef.getPublisher() != null) {
+        if (!"".equals(bookRef.getPublisher()) && bookRef.getPublisher() != null) {
             builder.append("publisher = {" + bookRef.getPublisher() + e);
         }
-        if (bookRef.getVolume() != "" && bookRef.getVolume() != null) {
+        if (!"".equals(bookRef.getVolume()) && bookRef.getVolume() != null) {
             builder.append("volume = {" + bookRef.getVolume() + e);
         }
-        if (bookRef.getSeries() != "" && bookRef.getSeries() != null) {
+        if (!"".equals(bookRef.getSeries()) && bookRef.getSeries() != null) {
             builder.append("series = {" + bookRef.getSeries() + e);
         }
-        if (bookRef.getAddress() != "" && bookRef.getAddress() != null) {
+        if (!"".equals(bookRef.getAddress()) && bookRef.getAddress() != null) {
             builder.append("address = {" + bookRef.getAddress() + e);
         }
-        if (bookRef.getEdition() != "" && bookRef.getEdition() != null) {
+        if (!"".equals(bookRef.getEdition()) && bookRef.getEdition() != null) {
             builder.append("edition = {" + bookRef.getEdition() + e);
         }
-        if (bookRef.getMonth() != "" && bookRef.getMonth() != null) {
+        if (!"".equals(bookRef.getMonth()) && bookRef.getMonth() != null) {
             builder.append("month = {" + bookRef.getMonth() + e);
         }
-        if (bookRef.getNote() != "" && bookRef.getNote() != null) {
+        if (!"".equals(bookRef.getNote()) && bookRef.getNote() != null) {
             builder.append("note = {" + bookRef.getNote() + e);
         }
-        if (bookRef.getKey() != "" && bookRef.getKey() != null) {
+        if (!"".equals(bookRef.getKey()) && bookRef.getKey() != null) {
             builder.append("key = {" + bookRef.getKey() + e);
         }
         builder.append(c);
@@ -110,34 +110,34 @@ public class Parser {
         builder.append("author = {" + reference.getAuthor() + e);
         builder.append("title = {" + reference.getTitle() + e);
         builder.append("year = {" + reference.getYear() + e);
-        if (reference.getEditor() != "" && reference.getEditor() != null) {
+        if (!"".equals(reference.getEditor()) && reference.getEditor() != null) {
             builder.append("editor = {" + reference.getEditor() + e);
         }
-        if (reference.getVolume() != "" && reference.getVolume() != null) {
+        if (!"".equals(reference.getVolume()) && reference.getVolume() != null) {
             builder.append("volume = {" + reference.getVolume() + e);
         }
-        if (reference.getSeries() != "" && reference.getSeries() != null) {
+        if (!"".equals(reference.getSeries()) && reference.getSeries() != null) {
             builder.append("series = {" + reference.getSeries() + e);
         }
-        if (reference.getPages() != "" && reference.getPages() != null) {
+        if (!"".equals(reference.getPages()) && reference.getPages() != null) {
             builder.append("pages = {" + reference.getPages() + e);
         }
-        if (reference.getAddress() != "" && reference.getAddress() != null) {
+        if (!"".equals(reference.getAddress()) && reference.getAddress() != null) {
             builder.append("address = {" + reference.getAddress() + e);
         }
-        if (reference.getMonth() != "" && reference.getMonth() != null) {
+        if (!"".equals(reference.getMonth()) && reference.getMonth() != null) {
             builder.append("month = {" + reference.getMonth() + e);
         }
-        if (reference.getOrganization() != "" && reference.getOrganization() != null) {
+        if (!"".equals(reference.getOrganization()) && reference.getOrganization() != null) {
             builder.append("organization = {" + reference.getOrganization() + e);
         }
-        if (reference.getPublisher() != "" && reference.getPublisher() != null) {
+        if (!"".equals(reference.getPublisher()) && reference.getPublisher() != null) {
             builder.append("publisher = {" + reference.getPublisher() + e);
         }
-        if (reference.getNote() != "" && reference.getNote() != null) {
+        if (!"".equals(reference.getNote()) && reference.getNote() != null) {
             builder.append("note = {" + reference.getNote() + e);
         }
-        if (reference.getKey() != "" && reference.getKey() != null) {
+        if (!"".equals(reference.getKey()) && reference.getKey() != null) {
             builder.append("key = {" + reference.getKey() + e);
         }
         builder.append(c);
@@ -149,25 +149,25 @@ public class Parser {
         builder.append("@booklet{");
         builder.append(reference.getLabel() + ",\n");
         builder.append("title = {" + reference.getTitle() + e);
-        if (reference.getAuthor() != "" && reference.getAuthor() != null) {
+        if (!"".equals(reference.getAuthor()) && reference.getAuthor() != null) {
             builder.append("author = {" + reference.getAuthor() + e);
         }
-        if (reference.getHowpublished() != "" && reference.getHowpublished() != null) {
+        if (!"".equals(reference.getHowpublished()) && reference.getHowpublished() != null) {
             builder.append("howpublished = {" + reference.getHowpublished() + e);
         }
-        if (reference.getAddress() != "" && reference.getAddress() != null) {
+        if (!"".equals(reference.getAddress()) && reference.getAddress() != null) {
             builder.append("address = {" + reference.getAddress() + e);
         }
-        if (reference.getMonth() != "" && reference.getMonth() != null) {
+        if (!"".equals(reference.getMonth()) && reference.getMonth() != null) {
             builder.append("month = {" + reference.getMonth() + e);
         }
         if (reference.getYear() > 0) {
             builder.append("year = {" + reference.getYear() + e);
         }
-        if (reference.getNote() != "" && reference.getNote() != null) {
+        if (!"".equals(reference.getNote()) && reference.getNote() != null) {
             builder.append("note = {" + reference.getNote() + e);
         }
-        if (reference.getKey() != "" && reference.getKey() != null) {
+        if (!"".equals(reference.getKey()) && reference.getKey() != null) {
             builder.append("key = {" + reference.getKey());
         }
         builder.append(c);
@@ -181,34 +181,34 @@ public class Parser {
         builder.append("author = {" + reference.getAuthor() + e);
         builder.append("title = {" + reference.getTitle() + e);
         builder.append("year = {" + reference.getYear() + e);
-        if (reference.getEditor() != "" && reference.getEditor() != null) {
+        if (!"".equals(reference.getEditor()) && reference.getEditor() != null) {
             builder.append("editor = {" + reference.getEditor() + e);
         }
-        if (reference.getVolume() != "" && reference.getVolume() != null) {
+        if (!"".equals(reference.getVolume()) && reference.getVolume() != null) {
             builder.append("volume = {" + reference.getVolume() + e);
         }
-        if (reference.getSeries() != "" && reference.getSeries() != null) {
+        if (!"".equals(reference.getSeries()) && reference.getSeries() != null) {
             builder.append("series = {" + reference.getSeries() + e);
         }
-        if (reference.getPages() != "" && reference.getPages() != null) {
+        if (!"".equals(reference.getPages()) && reference.getPages() != null) {
             builder.append("pages = {" + reference.getPages() + e);
         }
-        if (reference.getAddress() != "" && reference.getAddress() != null) {
+        if (!"".equals(reference.getAddress()) && reference.getAddress() != null) {
             builder.append("address = {" + reference.getAddress() + e);
         }
-        if (reference.getMonth() != "" && reference.getMonth() != null) {
+        if (!"".equals(reference.getMonth()) && reference.getMonth() != null) {
             builder.append("month = {" + reference.getMonth() + e);
         }
-        if (reference.getOrganization() != "" && reference.getOrganization() != null) {
+        if (!"".equals(reference.getOrganization()) && reference.getOrganization() != null) {
             builder.append("organization = {" + reference.getOrganization() + e);
         }
-        if (reference.getPublisher() != "" && reference.getPublisher() != null) {
+        if (!"".equals(reference.getPublisher()) && reference.getPublisher() != null) {
             builder.append("publisher = {" + reference.getPublisher() + e);
         }
-        if (reference.getNote() != "" && reference.getNote() != null) {
+        if (!"".equals(reference.getNote()) && reference.getNote() != null) {
             builder.append("note = {" + reference.getNote() + e);
         }
-        if (reference.getKey() != "" && reference.getKey() != null) {
+        if (!"".equals(reference.getKey()) && reference.getKey() != null) {
             builder.append("key = {" + reference.getKey() + e);
         }
         builder.append(c);
@@ -221,47 +221,47 @@ public class Parser {
         builder.append(reference.getLabel() + ",\n");
         builder.append("author = {" + reference.getAuthor() + e);
         builder.append("title = {" + reference.getTitle() + e);
-        if (reference.getBooktitle() != "" && reference.getBooktitle() != null) {
+        if (!"".equals(reference.getBooktitle()) && reference.getBooktitle() != null) {
             builder.append("booktitle = {" + reference.getBooktitle() + e);
         }
-        if(reference.getPublisher() != "" && reference.getPublisher() !=null){
+        if(!"".equals(reference.getPublisher()) && reference.getPublisher() !=null){
             builder.append("publisher = {" + reference.getPublisher() + e);
         }
         builder.append("year = {" + reference.getYear() + e);
-        if (reference.getEditor() != "" && reference.getEditor() != null) {
+        if (!"".equals(reference.getEditor()) && reference.getEditor() != null) {
             builder.append("editor = {" + reference.getEditor() + e);
         }
-        if (reference.getNumber() != "" && reference.getNumber() != null) {
+        if (!"".equals(reference.getNumber()) && reference.getNumber() != null) {
             builder.append("number = {" + reference.getNumber() + e);
         }
-        if (reference.getVolume() != "" && reference.getVolume() != null) {
+        if (!"".equals(reference.getVolume()) && reference.getVolume() != null) {
             builder.append("volume = {" + reference.getVolume() + e);
         }
-        if (reference.getSeries() != "" && reference.getSeries() != null) {
+        if (!"".equals(reference.getSeries()) && reference.getSeries() != null) {
             builder.append("series = {" + reference.getSeries() + e);
         }
-        if (reference.getIncollectiontype() != "" && reference.getIncollectiontype() != null) {
+        if (!"".equals(reference.getIncollectiontype()) && reference.getIncollectiontype() != null) {
             builder.append("type = {" + reference.getIncollectiontype() + e);
         }
-        if(reference.getChapter()!="" && reference.getChapter()!=null){
+        if(!"".equals(reference.getChapter()) && reference.getChapter()!=null){
             builder.append("chapter = {"+ reference.getChapter()+e);
         }
-        if(reference.getPages()!="" && reference.getPages()!=null){
+        if(!"".equals(reference.getPages()) && reference.getPages()!=null){
             builder.append("pages = {" +  reference.getPages()+e);
         }
-        if(reference.getAddress()!="" && reference.getAddress()!=null){
+        if(!"".equals(reference.getAddress()) && reference.getAddress()!=null){
             builder.append("address = {" + reference.getAddress()+e);
         }
-        if(reference.getEdition()!="" && reference.getEdition()!=null){
+        if(!"".equals(reference.getEdition()) && reference.getEdition()!=null){
             builder.append("edition = {" + reference.getEdition()+e);
         }
-        if(reference.getMonth()!="" && reference.getMonth()!=null){
+        if(!"".equals(reference.getMonth()) && reference.getMonth()!=null){
             builder.append("month = {"+reference.getMonth()+e);
         }
-        if(reference.getNote()!="" && reference.getNote()!=null){
+        if(!"".equals(reference.getNote()) && reference.getNote()!=null){
             builder.append("note = {"+reference.getNote()+e);
         }
-        if(reference.getKey()!="" && reference.getKey()!=null){
+        if(!"".equals(reference.getKey()) && reference.getKey()!=null){
             builder.append("key = {"+reference.getKey()+e);
         }
         builder.append(c);
