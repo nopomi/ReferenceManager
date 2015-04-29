19f7f97927313e0d1f0ef2a842b2e20bf63359a1
Virpi Sumu <virpi.sumu@gmail.com>
2015-04-29 13:53:39 +0300

    war toimii, easyb/jetty ei vieläkään...

--- ReferenceManager/src/main/java/wad/Application.java
+++ ReferenceManager/src/main/java/wad/Application.java
@@ -2,6 +2,8 @@ package wad;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -12,7 +14,12 @@ import wad.profiles.ProdProfile;
 @EnableAutoConfiguration
 @ComponentScan
 @Import({DevProfile.class, ProdProfile.class})
-public class Application {
+public class Application extends SpringBootServletInitializer{
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
